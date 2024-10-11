2d099e6781f7e48ed23960fe956bbbb3b550a01d
MegumiOhara <156937802+MegumiOhara@users.noreply.github.com>
2024-10-12 00:31:21 +0900

    installed axios and connect server to client
    to test it out

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,17 @@
+import axios from "axios";
+import { useEffect } from "react";
 
 function App() {
   
+  const fetchApi = async() => {
+    const response= await axios.get("http://localhost:8080/api");
+    console.log(response);
+  };
 
+  useEffect(() => {
+    fetchApi();
+  }, [])
+ 
   return (
     <div>
       <h1>Local Transport and Weather Dashboard</h1>
--- server/server.js
+++ server/server.js
@@ -5,12 +5,12 @@ const corsOptions = {
     origin: ["http://localhost:5173"],
 };
 
-//app.arguments(cors(corsOptions));
+app.use(cors(corsOptions));
 
 app.get("/api", (req,res) =>{
     res.json({ comment: ["example response"]});
 });
 
 app.listen(8080, () => {
-    console.log("Server is runnong on port 8080")
+    console.log("Server is running on port 8080")
 });
