f1b8a6e8ba65a96d3417f8f7ada01b4d0e598d5c
MegumiOhara <156937802+MegumiOhara@users.noreply.github.com>
2024-10-12 00:04:16 +0900

    added cors

--- server/server.js
+++ server/server.js
@@ -1,3 +1,8 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const corsOptions = {
+    origin: ["http://localhost:5173"],
+};
 
+app.arguments(cors(corsOptions));
