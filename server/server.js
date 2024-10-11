61d404d3f59513dfb433e176ace704d43b6b9d28
MegumiOhara <156937802+MegumiOhara@users.noreply.github.com>
2024-10-12 00:10:02 +0900

    added data on server to test it out

--- server/server.js
+++ server/server.js
@@ -5,4 +5,12 @@ const corsOptions = {
     origin: ["http://localhost:5173"],
 };
 
-app.arguments(cors(corsOptions));
+//app.arguments(cors(corsOptions));
+
+app.get("/api", (req,res) =>{
+    res.json({ comment: ["example response"]});
+});
+
+app.listen(8080, () => {
+    console.log("Server is runnong on port 8080")
+});
