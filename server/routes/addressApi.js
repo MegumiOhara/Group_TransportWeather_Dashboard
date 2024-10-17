8a291026c0785a96e24eeb585f66773250eb4f2f
MegumiOhara <156937802+MegumiOhara@users.noreply.github.com>
2024-10-17 09:36:40 +0200

    set up app and port

--- server/routes/addressApi.js
+++ server/routes/addressApi.js
@@ -2,3 +2,8 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 require('dotenv').config(); //to use environment variables.
+
+const app = express();
+const port = 3000;
+
+//Middleware
