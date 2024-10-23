8d7aab46e51aff184f38e1e703f93c834cceb6b7
MegumiOhara <156937802+MegumiOhara@users.noreply.github.com>
2024-10-23 11:31:41 +0200

    added customized screen sizes for styling

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
      "./src/**/*.{js,ts,jsx,tsx}", // Adjust based on your project structure
   ],
   theme: {
-     extend: {},
+     extend: {
+      screens: {
+        'sm': '481px',
+        //=> @media (min-width: 481px) for tablet
+
+        'md' : '1201px',
+        //=> @media (min-width: 1201px) for laptops
+
+        'lg' : '1601px',
+        //=> @mesia (min-width: 1601px) for desktops
+      }
+     },
   },
   plugins: [],
 };
