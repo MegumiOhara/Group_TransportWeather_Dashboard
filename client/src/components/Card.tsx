64ba8e867f1e0ce67fd957291d1e4009ddec0d46
Alejandra <monicsalejandra@gmail.com>
2024-10-23 21:17:10 +0200

    updated

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -3,12 +3,6 @@ import SpinnerW from './SpinnerW';
 
 const Card = ({ loadingData, showData, weather, forecast }) => {
 
-    var today = new Date();
-    var day = today.getDate();
-    var month = today.getMonth() + 1;
-    var year = today.getFullYear();
-    var date = day + '/' + month + '/' + year;
-
     var url = "";
     var iconUrl = "";
 
@@ -47,37 +41,26 @@ const Card = ({ loadingData, showData, weather, forecast }) => {
                         <div className="card mb-3 mx-auto bg-dark text-light">
                             <div className="row g-0">
                                 <div className="col-md-4">
+                                    <h4>LOCAL WEATHER</h4>
                                     <h3 className="card-title">{weather.name}</h3>
-                                    <p className="card-date">{date}</p>
-                                    <h1 className="card-temp">{(weather.main.temp - 273.15).toFixed(1)}ºC</h1>
-                                    <p className="card-desc"><img src={iconUrl} alt="icon" />{weather.weather[0].description}</p>
-                                    <img src="https://images.pexels.com/photos/10817264/pexels-photo-10817264.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" className="img-fluid rounded-start" alt="..." />
+
+
+
                                 </div>
                                 <div className="col-md-8">
                                     <div className="card-body text-start mt-2">
                                         <h5 className="card-text">Temperatura máxima: {(weather.main.temp_max - 273.15).toFixed(1)}ºC</h5>
                                         <h5 className="card-text">Temperatura mínima: {(weather.main.temp_min - 273.15).toFixed(1)}ºC</h5>
-                                        <h5 className="card-text">sensación térmica: {(weather.main.feels_like - 273.15).toFixed(1)}ºC</h5>
-                                        <h5 className="card-text">Humedad: {weather.main.humidity}%</h5>
-                                        <h5 className="card-text">Velocidad del viento: {weather.wind.speed}m/s</h5>
+
+
                                     </div>
                                     <hr />
 
                                     <div className="row mt-4">
                                         <div className="col">
-                                            <p>{forecastDate3}h</p>
+
                                             <p className="description"><img src={iconUrl3} alt="icon" />{forecast.list[1].weather[0].description}</p>
-                                            <p className="temp">{(forecast.list[1].main.temp - 273.15).toFixed(1)}ºC</p>
-                                        </div>
-                                        <div className="col">
-                                            <p>{forecastDate6}h</p>
-                                            <p className="description"><img src={iconUrl6} alt="icon" />{forecast.list[2].weather[0].description}</p>
-                                            <p className="temp">{(forecast.list[2].main.temp - 273.15).toFixed(1)}ºC</p>
-                                        </div>
-                                        <div className="col">
-                                            <p>{forecastDate9}h</p>
-                                            <p className="description"><img src={iconUrl9} alt="icon" />{forecast.list[3].weather[0].description}</p>
-                                            <p className="temp">{(forecast.list[3].main.temp - 273.15).toFixed(1)}ºC</p>
+
                                         </div>
 
 
