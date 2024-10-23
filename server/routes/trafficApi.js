17b0f1c2ef9f84ed728e4658b4340745eba6e444
Faustina-Acheampong <157317178+Faustina-Acheampong@users.noreply.github.com>
2024-10-24 01:36:29 +0200

    Add API key and URL to trafficApi.js

--- server/routes/trafficApi.js
+++ server/routes/trafficApi.js
@@ -1,5 +1,6 @@
 import express from "express";
 import axios from "axios";
+<<<<<<< Updated upstream
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -18,3 +19,13 @@ const API_URL = "https://api.trafikinfo.trafikverket.se/v2/data.json";
 
 
 export default router;
+=======
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+const PORT = 8080;
+
+app.use(cors());
+app.use(express.json());
+>>>>>>> Stashed changes
