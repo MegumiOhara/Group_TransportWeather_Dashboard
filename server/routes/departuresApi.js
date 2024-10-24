843afbfd467ea924a6843cf2ef86f48a49876469
Daniela Y. Zagal <149149177+danielaayz@users.noreply.github.com>
2024-10-24 09:55:42 +0200

    update departure route

--- server/routes/departuresApi.js
+++ server/routes/departuresApi.js
@@ -4,21 +4,17 @@ import dotenv from "dotenv";
 
 // Load environment variables from .env file
 dotenv.config();
-
-// Router
 const router = express.Router();
-
-// API-key
 const apiKey = process.env.RESROBOT_API_KEY;
 
-// Define base URL's
-const stationsProximityApiUrl = `https://api.resrobot.se/v2.1/location.nearbystops`;
+// Base URL's
+const nearbyStopsApiUrl = `https://api.resrobot.se/v2.1/location.nearbystops`;
 const departureBoardApiUrl = `https://api.resrobot.se/v2.1/departureBoard`;
 
 // Function to get nearest station ID based on coordinates
 const getNearestStationId = async (latitude, longitude) => {
    try {
-      const response = await axios.get(stationsProximityApiUrl, {
+      const response = await axios.get(nearbyStopsApiUrl, {
          params: {
             format: "json",
             accessId: apiKey,
@@ -34,7 +30,7 @@ const getNearestStationId = async (latitude, longitude) => {
          response.data.stopLocationOrCoordLocation.length > 0
       ) {
          // Return nearest station object
-         return response.data.stopLocationOrCoordLocation[0].StopLocation; // Return nearest station object
+         return response.data.stopLocationOrCoordLocation[0].StopLocation;
       } else {
          return null; // No station found
       }
@@ -44,65 +40,55 @@ const getNearestStationId = async (latitude, longitude) => {
    }
 };
 
-// Function uses the ID to fetch departure list for s specific station
-const getDepartureBoard = async (stationId) => {
+// Common function for making API request to Resrobot
+const fetchDataFromResRobot = async (url, params) => {
    try {
-      const params = {
-         accessId: apiKey,
-         id: stationId, // Mandatory: Station ID
-         format: "json", // Response format: JSON
-         duration: 60, // Time interval for departures
-         passlist: 0,
-         lang: "en", // Lang. set to English
-      };
-
-      // Make the API request
-      const response = await axios.get(departureBoardApiUrl, { params });
-
-      // Map the departures to a simplified format
-      const departures = response.data.Departure.map((departure) => {
-         const scheduledTime = departure.time.slice(0, 5); // Removes seconds from time
-         const realTime = departure.rtTime
-            ? departure.rtTime.slice(0, 5)
-            : "On time"; // Real-time departure in HH:MM
-
-         // Calculate the journey duration if realtime data available
-         let duration = null;
-         if (departure.rtTime && departure.time) {
-            const departureTime = new Date(
-               `${departure.date}T${departure.time}`
-            );
-            const realDepartureTime = new Date(
-               `${departure.rtDate || departure.date}T${departure.rtTime}`
-            );
-            const timeDifference =
-               Math.abs(realDepartureTime - departureTime) / 60000; // Difference in minutes
-            duration = `${timeDifference} min`;
-         }
-
-         return {
-            name: departure.name, // Transport name
-            scheduledTime,
-            realTime,
-            track: departure.rtDepTrack || "Unknown",
-            destination: departure.direction, // Final stop
-            duration: duration || "N/A",
-            line: departure.transportNumber, // Line number
-            operator: departure.ProductAtStop.operator || "Unknown", // Operator Name
-            type: departure.transportCategory, // Transport type
-         };
-      });
-
-      return departures;
+      const response = await axios.get(url, { params });
+      return response.data;
    } catch (error) {
-      console.error("Error fetching departure board:", error);
-      throw new Error("Error fetching departure board");
+      console.error(`Error fetching data from ${url}`, error);
+      throw new Error(`Error fetching data from ${url}`);
    }
+};
+
+// Function to fetch departure data
+const getDepartureBoard = async (stationId, products = 0) => {
+   const params = {
+      accessId: apiKey,
+      id: stationId,
+      format: "json",
+      duration: 60,
+      passlist: 0,
+      lang: "en",
+      products,
+   };
+
+   const data = await fetchDataFromResRobot(departureBoardApiUrl, params);
+
+   console.log("Departure data:", data); // Log the data for debugging
+
+   return data.Departure.map((departure) => {
+      const product = departure.ProductAtStop || {}; // Extract ProductAtStop for product info
+
+      return {
+         name: product.name || "Unknown",
+         displayNumber: product.displayNumber || "Unknown",
+         transportType: product.catOutL || "Unknown",
+         operator: product.operator || "Unknown",
+         scheduledTime: departure.time ? departure.time.slice(0, 5) : "Unknown",
+         realTime: departure.rtTime ? departure.rtTime.slice(0, 5) : "On time",
+         track: departure.rtTrack || "Unknown",
+         depTrack: departure.rtDepTrack || "Unknown",
+         line: departure.transportNumber,
+         type: departure.transportCategory,
+         destination: departure.direction,
+      };
+   }).filter((departure) => departure.scheduledTime);
 };
 
 // Unified route to handle both stations and departures based on coordinates
 router.post("/location", async (req, res) => {
-   console.log("Request received for departures", req.body);
+   console.log("Request received for departures and arrivals", req.body);
 
    // Extract latitude and longitude from request body
    const { latitude, longitude } = req.body;
@@ -114,24 +100,19 @@ router.post("/location", async (req, res) => {
    }
 
    try {
-      // Fetch nearby station object
+      // Fetch nearest station
       const nearestStation = await getNearestStationId(latitude, longitude);
 
       // 1. Check if any stations ID were found
       if (!nearestStation) {
-         return res
-            .status(400)
-            .json({ message: "No stations found near the given coordinates" });
+         return res.status(400).json({ message: "No stations found nearby" });
       }
-      // 2. Fetch departures for nearest station (stationID)
+      // 2. Fetch departures for nearest station
       const departures = await getDepartureBoard(nearestStation.id);
 
       // Send station info and departures as response
       return res.json({
-         nearestStation: {
-            name: nearestStation.name, // Return station's name
-            id: nearestStation.id, // Return the station's ID
-         },
+         station: nearestStation.name,
          departures,
       });
    } catch (error) {
