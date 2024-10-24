4e475ca79b18595c566371224bab6c6af16d84a5
Daniela Y. Zagal <149149177+danielaayz@users.noreply.github.com>
2024-10-24 15:55:06 +0200

    add icons

--- client/src/components/Departures.tsx
+++ client/src/components/Departures.tsx
@@ -1,98 +1,159 @@
-import React, { useState } from "react";
-import AddressInput from "./Address";
+import { useEffect, useState } from "react";
 import axios from "axios";
-
-// Interfaces for data types
-// interface Location {
-//    latitude: number;
-//    longitude: number;
-// }
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+   faBus,
+   faTrain,
+   faTrainTram,
+   faTrainSubway,
+   faShip,
+} from "@fortawesome/free-solid-svg-icons";
 
 interface Departure {
    name: string;
+   displayNumber: string;
+   transportType: string;
+   operator: string;
+   scheduledTime: string;
+   realTime: string;
+   track: string;
+   depTrack: string;
    line: string;
-   time: string;
+   type: string;
    destination: string;
+   arrivalTime: string;
+   duration: string;
 }
 
-interface StationInfo {
-   name: string;
-   id: string;
+interface DepartureProps {
    lat: number;
-   lon: number;
+   lng: number;
 }
 
-const Departures: React.FC = () => {
-   // State to store departures, station info, and error message
+function Departures({ lat, lng }: DepartureProps) {
    const [departures, setDepartures] = useState<Departure[]>([]);
+   const [loading, setLoading] = useState<boolean>(true);
    const [error, setError] = useState<string | null>(null);
-   const [stationInfo, setStationInfo] = useState<StationInfo | null>(null);
 
-   // Function to fetch departures based on latitude and longitude
-   const fetchDepartures = async (lat: number, lng: number): Promise<void> => {
-      try {
-         const response = await axios.post(
-            "http://localhost:3000/api/location",
-            {
-               latitude: lat,
-               longitude: lng,
-            }
-         );
+   useEffect(() => {
+      const fetchDepartures = async () => {
+         try {
+            const response = await axios.post(
+               "http://localhost:3000/api/location",
+               {
+                  latitude: lat,
+                  longitude: lng,
+               }
+            );
+            setDepartures(response.data.departures);
+            setLoading(false);
+         } catch (error) {
+            console.error("Error fetching departures:", error);
+            setError("Error fetching departures.");
+            setLoading(false);
+         }
+      };
 
-         const data = response.data;
+      fetchDepartures();
+   }, [lat, lng]);
 
-         setDepartures(data.departures); // Update departures state
-         setStationInfo(data.nearestStation); // Update station info state
-      } catch (error) {
-         console.error("Error fetching departures:", error);
-         setError("An error occurred while fetching departures");
+   // Function to format current date
+   const today = new Date();
+   const options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+   } as const;
+   const formattedDate = `${today.toLocaleDateString("en-US", options)}`;
+
+   // Function to return appropriate icon for transport type
+   const getTransportIcon = (type?: string) => {
+      if (!type) {
+         return <FontAwesomeIcon icon={faBus} className="text-gray-700" />; // Standard till bus-ikonen om `type` saknas
+      }
+      switch (type.toLowerCase()) {
+         case "bus":
+         case "regionbus":
+            return <FontAwesomeIcon icon={faBus} className="text-gray-700" />;
+         case "train":
+         case "pendeltåg":
+         case "tåg":
+            return <FontAwesomeIcon icon={faTrain} className="text-gray-700" />;
+         case "tram":
+         case "spårvagn":
+         case "tvärbanan":
+            return (
+               <FontAwesomeIcon icon={faTrainTram} className="text-gray-700" />
+            );
+         case "metro":
+         case "tunnelbana":
+            return (
+               <FontAwesomeIcon
+                  icon={faTrainSubway}
+                  className="text-gray-700"
+               />
+            );
+         case "ferry":
+         case "färja":
+            return <FontAwesomeIcon icon={faShip} className="text-gray-700" />;
+         default:
+            return <FontAwesomeIcon icon={faBus} className="text-gray-700" />; // Default to bus if type unknown
       }
    };
 
-   // Handle error from AddressInput component
-   const handleError = (errorMessage: string): void => {
-      setError(errorMessage);
-   };
+   if (loading) {
+      return <p>Loading departures...</p>;
+   }
+
+   if (error) {
+      return <p>{error}</p>;
+   }
 
    return (
-      <div>
-         <h1>Find Public Transport Departures</h1>
+      <>
+         <article className="max-w-md mx-auto p-4 border border-[#E4602F] rounded-md bg-white">
+            <h2 className="text-[#E4602F] font-lato text-base font-semibold mb-2">
+               Transport Departures
+            </h2>
+            <p className="text-black font-lato text-sm font-bold mb-4">
+               {formattedDate}
+            </p>
 
-         {/* AddressInput component to get coordinates based on address */}
-         <AddressInput
-            onGeocode={(lat, lng) => {
-               setError(null); // Clear any previous error
-               fetchDepartures(lat, lng); // Fetch departures for given coordinates
-            }}
-            onError={handleError}
-         />
-
-         {/* Show station information */}
-         {stationInfo && (
-            <div>
-               <h2>Nearest Station: {stationInfo.name}</h2>
-               <p>
-                  Location: {stationInfo.lat}, {stationInfo.lon}
-               </p>
-            </div>
-         )}
-
-         {/* Display departures or error */}
-         {error && <p style={{ color: "red" }}>{error}</p>}
-         {departures.length > 0 ? (
-            <ul>
-               {departures.map((departure, index) => (
-                  <li key={index}>
-                     {departure.name} - {departure.time} -{" "}
-                     {departure.destination}
-                  </li>
+            <div className="space-y-4">
+               {departures.slice(0, 5).map((departure, i) => (
+                  <div
+                     key={i}
+                     className="p-4 border-t border-b border-gray-400 bg-white">
+                     <div className="flex items-center justify-between mb-2">
+                        <span className="font-bold text-lg">
+                           {departure.scheduledTime} {departure.realTime}
+                        </span>
+                        <span className="text-sm text-gray-500">
+                           {departure.duration}
+                        </span>
+                        <div className="text-base font-medium mb-1">
+                           {departure.name} - {departure.destination}
+                           <span className="text-sm text-gray-500">
+                              {departure.track || departure.depTrack}
+                           </span>
+                        </div>
+                        <div className="flex items-center space-x-2 text-gray-600">
+                           <span className="inline-flex items-center justify-center w-8 h-8 bg-gray-300 rounded-full p-2">
+                              {getTransportIcon(departure.type)}
+                           </span>
+                           <span className="bg-gray-200 px-2 py-1 rounded-full text-xs">
+                              {departure.transportType}{" "}
+                              {departure.displayNumber}
+                           </span>
+                        </div>
+                     </div>
+                  </div>
                ))}
-            </ul>
-         ) : (
-            !error && <p>No departures found.</p>
-         )}
-      </div>
+            </div>
+         </article>
+      </>
    );
-};
+}
 
 export default Departures;
