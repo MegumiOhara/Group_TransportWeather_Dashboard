d1bd6697e50f456fc9449cf8b2023ff53b7bfb0e
Alejandra <monicsalejandra@gmail.com>
2024-10-25 21:38:38 +0200

    finished with the desing and js

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import SpinnerW from './SpinnerW';
 
-// Función para obtener los nombres de los días de la semana, empezando por el día actual
+// function of days of week
 const getWeekDays = () => {
-    return ['Today', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const daysOfWeek = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+    const today = new Date().getDay(); // Obtain the actual day 
+
+
+    const orderedDays = [...daysOfWeek.slice(today), ...daysOfWeek.slice(0, today)];
+
+    orderedDays[0] = 'Today';
+
+    return orderedDays;
 };
 
 const Card = ({ loadingData, showData, weather, forecast }) => {
@@ -17,37 +25,52 @@ const Card = ({ loadingData, showData, weather, forecast }) => {
 
     const url = "http://openweathermap.org/img/w/";
     const weekDays = getWeekDays();
-
-    // Obtener pronósticos para los próximos 7 días
-    const forecastData = forecast.list.slice(0, 7); // Solo tomamos los primeros 7 días
+    const forecastData = forecast.list.slice(0, 7); // only 7 days
 
     return (
         <div className="mt-5">
             <div className="container">
-                <div className="card mb-3 mx-auto bg-light text-dark weather-card">
-                    <h4 className='title'>Local Weather</h4> {/* Título principal */}
-                    <div className="card-body p-2">
+                <div className="card mb-3 mx-auto">
+                    <div className="card-body">
+                        <div className="card-header">
+                            <h4 className="title">Local Weather</h4>
+                            <div className="temperature">
+                                <h5 className="card-text">
+                                    Max: {(weather.main.temp_max - 273.15).toFixed(1)}ºC
+                                </h5>
+                                <h5 className="card-text">
+                                    Min: {(weather.main.temp_min - 273.15).toFixed(1)}ºC
+                                </h5>
+                            </div>
+                        </div>
 
-                        {/* Mostrar pronósticos en filas */}
-                        <div className="card-content"> {/* Contenedor para el contenido */}
-                            {forecastData.map((forecastItem, index) => (
-                                <div key={index} className="weather-row" style={{ justifyContent: 'center' }}>
-                                    {/* Columna para el día */}
-                                    <div className="week">{weekDays[index]}</div>
+                        {/* Container of the days and forecast */}
+                        <div className="weather-container">
+                            {/* Column of days of the week */}
+                            <div className="week">
+                                {weekDays.map((day, index) => (
+                                    <div key={index} className="day">{day}</div>
+                                ))}
+                            </div>
 
-                                    {/* Ícono y probabilidad de lluvia */}
-                                    <div className="weather-icon">
-                                        <img src={`${url}${forecastItem.weather[0].icon}.png`} alt="icon" />
-                                        <p className="pop">{(forecastItem.pop * 100).toFixed(0)}%</p> {/* Porcentaje al lado del ícono */}
+                            {/* Column of forecast */}
+                            <div className="weather-forecast">
+                                {forecastData.map((forecastItem, index) => (
+                                    <div key={index} className="weather-row">
+                                        <div className="weather-icon">
+                                            <img
+                                                src={`${url}${forecastItem.weather[0].icon}.png`}
+                                                alt="icon"
+                                            />
+                                            <p className="pop">{(forecastItem.pop * 100).toFixed(0)}%</p>
+                                        </div>
+                                        <div className="temperature">
+                                            <p className="temp-min">↓ {(forecastItem.main.temp_min - 273.15).toFixed(1)}ºC</p>
+                                            <p className="temp-max">↑ {(forecastItem.main.temp_max - 273.15).toFixed(1)}ºC</p>
+                                        </div>
                                     </div>
-
-                                    {/* Temperaturas máxima y mínima */}
-                                    <div className="temperature">
-                                        <p className="temp-min">↓ {(forecastItem.main.temp_min - 273.15).toFixed(1)}ºC</p>
-                                        <p className="temp-max">↑ {(forecastItem.main.temp_max - 273.15).toFixed(1)}ºC</p>
-                                    </div>
-                                </div>
-                            ))}
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -57,3 +80,4 @@ const Card = ({ loadingData, showData, weather, forecast }) => {
 };
 
 export default Card;
+
