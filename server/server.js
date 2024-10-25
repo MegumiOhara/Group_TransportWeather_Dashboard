5c891951fc1c71c18f9bb2fd9a418d29a59c9a71
Faustina-Acheampong <157317178+Faustina-Acheampong@users.noreply.github.com>
2024-10-25 06:28:57 +0200

    Imported dotenv.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // Change to import
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 //const express = require("express");
 //const cors = require("cors");
 import departuresApi from "./routes/departuresApi.js";
