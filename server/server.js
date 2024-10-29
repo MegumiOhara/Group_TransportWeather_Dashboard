abaea06c2fec2a8ab1c6a0ff0b33c71c90265de7
Daniela Y. Zagal <149149177+danielaayz@users.noreply.github.com>
2024-10-29 13:54:44 +0100

    add deleted import

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import departuresApi from "./routes/departuresApi.js";
 import addressApi from "./routes/addressApi.js";
-import dotenv from "dotenv";
-import trafficApi from "./routes/trafficApi.js";
+//import trafficApi from "./routes/trafficApi.js";
 
 dotenv.config(); //Load env variables from .env file
 
@@ -11,6 +11,8 @@ const app = express();
 const port = 3000;
 const corsOptions = {
    origin: ["http://localhost:5173"],
+   methods: ["GET", "POST", "PUT", "DELETE"],
+   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 app.use(cors(corsOptions));
@@ -18,13 +20,13 @@ app.use(express.json());
 
 app.use((req, res, next) => {
    console.log(`${req.method} ${req.url}`);
-   next(); // Gå vidare till nästa middleware eller route
+   next();
 });
 
 // Use the departures API routes
 app.use("/api", departuresApi);
 app.use("/api/address", addressApi);
-app.use("/api/traffic", trafficApi);
+//app.use("/api/traffic", trafficApi);
 
 app.get("/api", (req, res) => {
    res.json({ comment: ["example response"] });
